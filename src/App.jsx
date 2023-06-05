28c2be4384cf67f312cbb1e1c3453ddf56c14e4f
muhammed danish <muhammeddanishu@gmail.com>
2023-06-05 14:17:44 +0530

    t max first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import Home from './pages/Home'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
